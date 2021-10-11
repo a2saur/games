416738800a79baf6e935d345e3c5a914ca6ee0f3
a2saur <a2_shop@atsaur.com>
2021-10-11 09:44:07 -0400

    Added Changes

--- gravity.js
+++ gravity.js
@@ -2,11 +2,11 @@
 const cvs = document.getElementById("game");
 const ctx = cvs.getContext("2d");
 
-const cGRAVITY = 1;
+const cGRAVITY = 3;
 
 // CLASSES
 class Sprite {
-    constructor (x, y, width, height, root, animation_info, gravity_affected=false, solid=true) {
+    constructor (x, y, width, height, root, animation_info, gravity_affected=false, solid=true, friction=0) {
         this.root = root;
         this.animation_info = animation_info;
         this.width = width;
@@ -27,6 +27,10 @@ class Sprite {
             vX:0, 
             vY:0
         }
+
+        this.friction = friction;
+        this.acted_friction = 0;
+        this.grounded = false;
     }
 
     update (frames, animation="default", reverse=false, solid_items=[]) {
@@ -35,7 +39,9 @@ class Sprite {
         let frame_wait = this.animation_info[animation][1];
 
         this.currentImg.src = this.root+animation+"-"+this.current_frame.toString()+".png";
-
+        ctx.strokeStyle = '#00f';
+        ctx.lineWidth = 5;
+        ctx.strokeRect(this.pos.x, this.pos.y, this.width, this.height);
         ctx.drawImage(this.currentImg, this.pos.x, this.pos.y);
         
         if (reverse){
@@ -59,12 +65,15 @@ class Sprite {
         let goX = true;
         let temp_y = 0;
         let temp_x = 0;
+        this.acted_friction = 0;
+        this.grounded = false;
         //      check collision
         for (let x = 0; x < solid_items.length; x++){
-            if (solid_items[x].pos.x < this.pos.x &&
-                solid_items[x].pos.x + solid_items[x].width > this.pos.x &&
-                solid_items[x].pos.y < this.pos.y+this.velocity.vY+this.height &&
-                solid_items[x].height + solid_items[x].pos.y > this.pos.y+this.height) {
+            // if (solid_items[x].pos.x < this.pos.x &&
+            //     solid_items[x].pos.x + solid_items[x].width > this.pos.x &&
+            //     solid_items[x].pos.y < this.pos.y+this.velocity.vY+this.height &&
+            //     solid_items[x].height + solid_items[x].pos.y > this.pos.y+this.height) {
+            if (solid_items[x].collides_with({pos:{x:this.pos.x, y:this.pos.y}, width:this.width, height:this.velocity.vY+this.height})){
                 // collision detected!
                 goY = false;
                 if (solid_items[x].pos.y > this.pos.y){
@@ -72,11 +81,14 @@ class Sprite {
                 } else {
                     temp_y = solid_items[x].pos.y+solid_items[x].height;
                 }
+                this.acted_friction = solid_items[x].friction
+                this.grounded = true;
             }
-            if (solid_items[x].pos.x < this.pos.y &&
-                solid_items[x].pos.x + solid_items[x].width > this.pos.y &&
-                solid_items[x].pos.y < this.pos.x+this.velocity.vX+this.width &&
-                solid_items[x].height + solid_items[x].pos.y > this.pos.x+this.width) {
+            // if (solid_items[x].pos.y < this.pos.y &&
+            //     solid_items[x].pos.y + solid_items[x].height > this.pos.y &&
+            //     solid_items[x].pos.x < this.pos.x+this.velocity.vX+this.width &&
+            //     solid_items[x].height + solid_items[x].pos.x > this.pos.x+this.width) {
+            if (solid_items[x].collides_with({pos:{x:this.pos.x, y:this.pos.y}, width:this.velocity.vX+(this.width/2), height:this.height})){
                 // collision detected!
                 goX = false;
                 if (solid_items[x].pos.x > this.pos.x){
@@ -103,10 +115,19 @@ class Sprite {
         if (this.gravity_affected && goX && goY){
             this.velocity.vY += cGRAVITY;
         }
-
-        ctx.font = "30px Arial";
-        ctx.fillStyle = "#000";
-        ctx.fillText("Velocity: "+this.velocity.vX.toString(), 400, 400);
+        if (this.velocity.vX < 0) {
+            if (this.velocity.vX+this.acted_friction > 0) {
+                this.velocity.vX = 0;
+            } else {
+                this.velocity.vX += this.acted_friction
+            }
+        } else if (this.velocity.vX > 0) {
+            if (this.velocity.vX-this.acted_friction < 0) {
+                this.velocity.vX = 0;
+            } else {
+                this.velocity.vX -= this.acted_friction
+            }
+        }
     }
 
     collides_with(sprite2){
@@ -151,30 +172,41 @@ const ghostR1 = new Image();
 ghostR1.src = "./Images/ghost-right-1.png";
 
 //Get Keys
-document.addEventListener("keydown", direction);
+document.addEventListener("keydown", direction); //keydown keypress keyup
 
 function direction(event){
     if (event.keyCode == 38){
         //up
-        current_anim = "jump";
-        char.move(0, -10);
+        current_anim = "default";
+        if (char.grounded){
+            char.move(0, -25);
+        }
     } if (event.keyCode == 40){
         //down
-        char.move(0, 10);
-        current_anim = "idle"
+        current_anim = "default"
+        if (char.grounded){
+            char.move(0, 25);
+        }
     } if (event.keyCode == 37){
         //left
-        current_anim = "walk";
-        char.pos.x -= 20;
+        current_anim = "default";
+        char.move(-10, 0);
+        // char.pos.x -= 25;
     } if (event.keyCode == 39){
         //right
-        current_anim = "walk";
-        char.pos.x += 20;
+        current_anim = "default";
+        char.move(10, 0);
+        // char.pos.x += 25;
     }
 }
 
 let char = new Sprite(200, 200, 50, 50, "./Images/gravity/me-", {"default":[1, 5], "idle":[8, 3], "walk":[4, 1], "jump":[11, 1],}, true, true);
-let block = new Sprite(0, 300, 700, 50, "./Images/gravity/me-", {"default":[1, 5]}, false, true);
+let block1 = new Sprite(100, 300, 50, 50, "./Images/gravity/dirt-", {"default":[1, 5]}, false, true, 100);
+let block2 = new Sprite(150, 300, 50, 50, "./Images/gravity/dirt-", {"default":[1, 5]}, false, true, 100);
+let block3 = new Sprite(200, 300, 50, 50, "./Images/gravity/dirt-", {"default":[1, 5]}, false, true, 100);
+let block4 = new Sprite(300, 300, 50, 50, "./Images/gravity/ice-", {"default":[1, 5]}, false, true, 1);
+let block5 = new Sprite(350, 300, 50, 50, "./Images/gravity/ice-", {"default":[1, 5]}, false, true, 1);
+let block6 = new Sprite(400, 300, 50, 50, "./Images/gravity/ice-", {"default":[1, 5]}, false, true, 1);
 // let char2 = new Sprite(400, 200, 50, 50, "./Images/gravity/me-", {"default":[1, 5], "idle":[8, 3], "walk":[4, 1], "jump":[11, 1],});
 let frames = 0;
 let current_anim = "idle"
@@ -185,10 +217,14 @@ function draw(){
     ctx.fillStyle = "#ccc";
     ctx.fillRect(0, 0, 800, 500);
 
-    ctx.drawImage(ghostR1, 0, 20);
-    char.update(frames, current_anim, false, [block]);
+    char.update(frames, current_anim, false, [block1, block2, block3, block4, block5, block6]);
     // document.write("Cool")
-    block.update(frames);
+    block1.update(frames);
+    block2.update(frames);
+    block3.update(frames);
+    block4.update(frames);
+    block5.update(frames);
+    block6.update(frames);
     // char2.update(frames, "idle");
     // Done?
     // if (enemy.y-player.y <= 20){
