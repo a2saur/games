d577a56e610f0b6d31664d064e4c3275c4c1a8ee
a2saur <a2_shop@atsaur.com>
2022-04-08 15:15:03 -0400

    Version 1.3: Added opening screen

--- collection_2.js
+++ collection_2.js
@@ -885,28 +885,28 @@ function draw(){
 
     if (current_scene == "opening"){
         ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, 288, 288);
-    
-    frames++;
-    ctx.drawImage(openingImg1, 0, 0, 4096/10, 3072/10);
-    ctx.drawImage(openingImg2, opening_pos.x, opening_pos.y, 4096/5, 3072/5);
+        ctx.fillRect(0, 0, 288, 288);
+        
+        frames++;
+        ctx.drawImage(openingImg1, 0, 0, 4096/10, 3072/10);
+        ctx.drawImage(openingImg2, opening_pos.x, opening_pos.y, 4096/5, 3072/5);
 
-    opening_pos.x += 4.7/2;
-    opening_pos.y += 1/2;
-    
-    if (opening_pos.x == 0 || opening_pos.y == 0){
-        opening_pos = {
-            x:-470,
-            y:-100,
+        opening_pos.x += 4.7/2;
+        opening_pos.y += 1/2;
+        
+        if (opening_pos.x == 0 || opening_pos.y == 0){
+            opening_pos = {
+                x:-470,
+                y:-100,
+            }
         }
-    }
 
-    ctx.fillStyle = "#EEF";
-    ctx.fillRect(0, 288, 288, 32);
+        ctx.fillStyle = "#EEF";
+        ctx.fillRect(0, 288, 288, 32);
 
-    ctx.font = "15px Arial";
-    ctx.fillStyle = opening_colors[parseInt(frames/3)%opening_colors.length];
-    ctx.fillText("Collection Game - Press Space to Start", 10, 310);
+        ctx.font = "15px Arial";
+        ctx.fillStyle = opening_colors[parseInt(frames/3)%opening_colors.length];
+        ctx.fillText("Collection Game - Press Space to Start", 10, 310);
     } else if (current_scene == "town"){
         // draw background
         counter = 0;
